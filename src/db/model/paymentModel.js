828e9be2952f1449358ee8845aed34732e75cff4
ogechukwu-Dev <josephine@lintechgroup.co.uk>
2024-02-18 21:50:21 +0100

    hotfix

--- src/db/model/paymentModel.js
+++ src/db/model/paymentModel.js
@@ -3,7 +3,7 @@ const TABLE = 'payments';
 
 class PaymentModel {
     async createPayment(payment_data) {
-        return db(TABLE).create(payment_data);
+        return db(TABLE).insert(payment_data);
     }
 
     async updatePayment(where_data,update_data){
